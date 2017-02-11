d00529e8e2428f2e5de9553f9837d0299e2aadaa
alexander <as@kodix.ru>
2017-02-11 14:51:21 +0300

    tmp

---
+++ src/components/AddCommentForm.js
@@ -0,0 +1,33 @@
+import React, { PropTypes, Component } from 'react';
+import {findDOMNode} from 'react-dom';
+
+
+class AddCommentForm extends Component{
+    /*state = {
+        user: '',
+        text: '',
+        id:0
+    }*/
+
+    render(){
+        return(
+            <form ref={this.getFormRef} onSubmit={this.handleSubmit}>
+                <input placeholder="user name" type="text"/><br />
+                <textarea placeholder="comment"></textarea><br />
+                <button type='submit'>Add comment</button>
+            </form>
+        )
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({});
+        findDOMNode(this.commentForm).reset();
+    }
+
+    getFormRef = (ref) => {
+        this.commentForm = ref;
+    }
+}
+
+export default AddCommentForm;
--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,21 @@ import Chart from './Chart'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 
+import DayPicker, { DateUtils } from "react-day-picker";
+import 'react-day-picker/lib/style.css';
+
 class App extends Component {
     state = {
         user: '',
-        selection: null
+        selection: null,
+        from: null,
+        to: null,
     }
 
     render() {
-        const {articles} = this.props
+        const {articles} = this.props;
+        const { from, to } = this.state;
+
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
@@ -20,10 +27,30 @@ class App extends Component {
             <div>
                 User: <input type="text" value={this.state.user} onChange={this.handleUserChange}/>
                 <Select options = {options} onChange={this.handleSelectChange} value={this.state.selection} multi/>
+                <br /><br /><br />
+                { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+                { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+                { from && to &&
+                <p>
+                    You chose from { moment(from).format('L') } to { moment(to).format('L') }.
+                    { ' ' }<a href="." onClick={ this.handleResetClick }>Reset</a>
+                </p>
+                }
+                <DayPicker
+                    numberOfMonths={ 1 }
+                    selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                    onDayClick={ this.handleDayClick }
+                />
+                <br /><br /><br />
                 <ArticleList articles={articles}/>
                 <Chart articles={articles}/>
             </div>
         )
+    }
+
+    handleDayClick = (e, day) => {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
     }
 
     handleSelectChange = selection => this.setState({ selection })
