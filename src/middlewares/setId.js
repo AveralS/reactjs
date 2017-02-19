0021900733f5527130624a56125323970929cf51
Roman <kaboom.ua@gmail.com>
2017-02-19 19:02:41 +0200

    Update setId.js

--- src/middlewares/setId.js
+++ src/middlewares/setId.js
@@ -2,6 +2,7 @@ import randomstring from 'randomstring';
 import {ADD_COMMENT} from '../constants'
 export default store => next => action => {
     const {type, payload, ...rest} = action;
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
     if(type == ADD_COMMENT){
         return next({...rest, type: ADD_COMMENT, payload: {...payload, id: randomstring.generate()}})
     }
